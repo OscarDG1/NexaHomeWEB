6e4d5d852095c6420a535622119a0f4327037480
OscarDG1 <122374560+OscarDG1@users.noreply.github.com>
2024-04-10 20:10:02 +0200

    Delete styles directory

--- styles/AppStyles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export const appStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
