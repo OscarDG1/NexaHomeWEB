73e012e450dd48b6f13a5d1de923e33157bf1724
Javier Ruiz <jaruiz@iris-eng.com>
2024-05-29 22:40:36 +0200

    Funciona gracias a dios

--- src/screens/change-password.jsx
+++ src/screens/change-password.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import NavigationBar from './NavigationBar'; // Suponiendo que el componente NavigationBar está en el archivo NavigationBar.js
+import NavigationBar from './NavigationBar';
 import '../styles/password.css';
 
-const API_BASE_URL = 'http://192.168.0.23:7770'; // Reemplaza 'tu-servidor.com' con la URL de tu servidor
+const API_BASE_URL = 'http://192.168.0.23:7770';
 
 function ChangePassword() {
   const [oldPassword, setOldPassword] = useState('');
@@ -23,13 +23,11 @@ function ChangePassword() {
         throw new Error('Usuario no autenticado');
       }
 
-      const response = await fetch(`${API_BASE_URL}/user/changePass`, {
+      const response = await fetch(`${API_BASE_URL}/user/changePass?oldPassword=${oldPassword}&newPassword=${newPassword}`, {
         method: 'GET',
         headers: {
-          'Content-Type': 'application/json',
           'Authorization': token,
         },
-        body: JSON.stringify({ oldPassword, newPassword })
       });
 
       if (!response.ok) {
@@ -37,7 +35,7 @@ function ChangePassword() {
       }
 
       alert('Contraseña cambiada exitosamente');
-      navigate('/perfil');
+      navigate('/');
     } catch (error) {
       console.error('Error al cambiar la contraseña:', error.message);
       alert(error.message);
